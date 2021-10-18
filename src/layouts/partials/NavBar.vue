499bfc8cfe5bb0320a0d99e49f729433a8fec635
Iago da Conceição Barbosa <iago.barbosa@ses.ba>
2021-10-18 13:45:51 -0300

    Realiza mudança no layout do navbar

--- src/layouts/partials/NavBar.vue
+++ src/layouts/partials/NavBar.vue
@@ -1,91 +1,68 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #F0E68C;">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#"><b>ControlCell</b></a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarScroll"
-          aria-controls="navbarScroll"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarScroll">
-          <ul
-            class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
-            style="--bs-scroll-height: 100px"
-          >
-            <li class="nav-item">
-              <router-link :to="{name: 'home'}" class="nav-link active"><i class="fas fa-home"></i> Home</router-link>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarScrollingDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-              <i class="bi bi-folder-plus"></i>
-              <i class="fas fa-plus-square"></i> Cadastros
-              </a>
-              <ul
-                class="dropdown-menu"
-                aria-labelledby="navbarScrollingDropdown"
-              >
-                <li><router-link :to="{name: 'customer'}" class="dropdown-item">Cientes</router-link></li>
-                <li><a class="dropdown-item" href="#">Produtos</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarScrollingDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-              <i class="fas fa-cogs"></i> Segurança
-              </a>
-              <ul
-                class="dropdown-menu"
-                aria-labelledby="navbarScrollingDropdown"
-              >
-                <li><a class="dropdown-item" href="#">Perfis</a></li>
-                <li><a class="dropdown-item" href="#">Usuários</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <form class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Pesquise"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-dark" type="submit">
-              Pesquisar
-            </button>
-          </form>
+    <nav
+        class="navbar navbar-dark bg-dark d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                <use xlink:href="#bootstrap"></use>
+            </svg>
+          <a class="navbar-brand" href="#"><b>Control</b><b style="color:#FFCA2C">Cell</b></a>
+        </a>
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li>
+            <router-link :to="{name: 'home'}" class="nav-link active px-2 link-dark"><i class="fas fa-home fa-lg"></i> Home</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle link-light"
+              href="#"
+              id="navbarScrollingDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+            Cadastros
+            </a>
+            <ul
+              class="dropdown-menu"
+              aria-labelledby="navbarScrollingDropdown"
+            >
+              <li><router-link :to="{name: 'customer'}" class="dropdown-item"><i class="fas fa-handshake"></i> Clientes</router-link></li>
+              <li><a class="dropdown-item" href="#"><i class="fas fa-box-open"></i> Produtos</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle link-light"
+              href="#"
+              id="navbarScrollingDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+            Segurança 
+            </a>
+            <ul
+              class="dropdown-menu"
+              aria-labelledby="navbarScrollingDropdown"
+            >
+              <li><router-link :to="{name: 'customer'}" class="dropdown-item"><i class="fas fa-user-tag"></i> Perfis</router-link></li>
+              <li><a class="dropdown-item" href="#"><i class="fas fa-users"></i> Usuários</a></li>
+            </ul>
+          </li>
+        </ul>
+        <div class="col-md-3 text-end">
+            <button type="button" class="btn btn-outline-warning me-2">Login</button>
         </div>
-      </div>
     </nav>
   </div>
 </template>
 <script>
 
 </script>
+<style>
+.active {
+  background: #FFCA2C;
+  border-radius: 10px;
+}
+</style>
--- src/pages/Customer/List.vue
+++ src/pages/Customer/List.vue
@@ -1,35 +1,41 @@
 <template>
-  <div class="container container-body">
-    <h3 class="my-4 title">Clientes</h3>
-    <table class="table table-striped table-bordered">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="container container-body mt-5">
+    <div class="card p-4" style="background: #FFFFFF">
+      <div class="d-flex justify-content-between">
+        <h3>Clientes</h3>
+        <button type="button" class="btn btn-warning btn-sm">Adicionar Cliente <i class="fas fa-plus"></i></button>
+      </div>
+      <hr>
+      <table class="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 <script>
